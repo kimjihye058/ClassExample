1630aeeb17dd9012c4402230d971915e4965b5d7
kimjihye058 <s2321@e-mirim.hs.kr>
2025-06-30 13:53:40 +0900

    메서드 하이딩, 오버라이딩 비교 구현

--- Program.cs
+++ Program.cs
@@ -121,7 +121,17 @@ namespace ClassExample
             child3.MethodO();        // 자식의 메소드 출력
             ((Parent)child3).MethodO();      // 자식의 메소드 출력
 
-
+            // 메서드 하이딩, 오버라이딩 비교 구현
+            List<Animal> Animals = new List<Animal>()
+            {
+                new Dog(), new Cat(), new Cat(), new Dog(),
+                new Dog(), new Cat(), new Dog(), new Dog()
+            };
+            foreach (var item in Animals)
+            {
+                item.Eat();
+            }
+            
         }
 
     }
